f932236c580ff109a8b67120e890679da8dc5cf2
Flávio Borges <flyfmb@hotmail.com>
2021-10-21 22:24:18 -0300

    Configurado o banco de dados em schema, init e app Close #6

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask, render_template
+import os, datetime  #para manipulação de datas
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}", format(os.path.join(project_dir, "database.db"))
+
 app = Flask('__name__')
+app.config['SECRET_KEY'] = 'your secret key'
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    title = db.Column(db.String(200), nullable=False)
 
 @app.route('/')
 def index():
