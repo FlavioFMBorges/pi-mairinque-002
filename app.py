0dc26ad5bfefb07c96a974245e57e1075f598cc5
Flávio Borges <flyfmb@hotmail.com>
2021-11-27 21:27:23 -0300

    Colocar gráfico que se atualiza a cada envio do formulário #56

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
+import matplotlib.pyplot as plt
+
 
 
 def get_db_connection():
@@ -152,6 +154,18 @@ def relatorio():
     else:
         meio_fraude = ('um site da internet')
 
+    plt.rcParams.update({'font.size': 10})
+    rotulos = ['18 a 27 anos', '28 a 39 anos', '40 a 55 anos', 'acima 55 anos']
+    valores = [dezoito_porc, vinteoito_porc, quarenta_porc, cinquenta_porc]
+    c = ['#ddb9b2', '#c2c9cd', '#4a8ab7', '#525e75']
+    explode = (.1, 0, .1, 0)
+
+    plt.figure(figsize=(8, 8))
+
+    plt.pie(x=valores, labels=rotulos, autopct='%1.1f%%', colors=c, shadow=True, explode=explode)
+    plt.savefig('static/imagens/diagrama-pizza.png')
+    plt.show()
+    plt.close()
 
     conn.commit()
     conn.close()
