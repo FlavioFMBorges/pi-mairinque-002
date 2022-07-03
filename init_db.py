243e52affcaf02851aa90cac8b10c4da554e51cd
pedro <opedrolps@gmail.com>
2022-07-02 23:17:34 -0300

    atualiza env vars

--- init_db.py
+++ init_db.py
@@ -3,10 +3,10 @@ import pymysql.cursors
 import os
 def get_db_connection():
     return pymysql.connect(
-        host= os.environ('HOST'),
+        host= os.environ['HOST'],
         port = 3306,
-        user = os.environ('USER'),
-        password = os.environ('PASSWORD'),
+        user = os.environ['USER'],
+        password = os.environ['PASSWORD'],
         cursorclass=pymysql.cursors.DictCursor,
         db='sys'
     )
