5d6844e50c720b1bd4f7ee7d395082c64be771a2
pedro <opedrolps@gmail.com>
2022-07-02 23:06:19 -0300

    adiciona variáveis de ambiente

--- init_db.py
+++ init_db.py
@@ -3,11 +3,10 @@ import pymysql.cursors
 
 def get_db_connection():
     return pymysql.connect(
-        host= 'database-2.c05d0e8qcds2.sa-east-1.rds.amazonaws.com',
+        host= HOST,
         port = 3306,
-        user ='admin',
-        password ='projeto2',
+        user = USER,
+        password = PASSWORD,
         cursorclass=pymysql.cursors.DictCursor,
         db='sys'
     )
-
